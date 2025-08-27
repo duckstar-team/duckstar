cc59d30e07eddb765cc9716e5d93bcd5e58fa80e
Godot-B <bae0821@gmail.com>
2025-08-27 22:09:19 +0900

    ci: -test.yml 수정

--- backend/src/main/java/com/duckstar/security/config/SecurityConfig.java
+++ backend/src/main/java/com/duckstar/security/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -23,6 +24,7 @@ import java.util.List;
 
 @EnableWebSecurity
 @Configuration
+@Profile("prod")
 @RequiredArgsConstructor
 @Slf4j
 public class SecurityConfig {
