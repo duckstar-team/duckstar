e6b99c6aac4dafdd8323f1631cd17b7dbb75930d
Godot-B <bae0821@gmail.com>
2025-09-27 21:43:36 +0900

    ci: 배포 준비 작업 (#70)

--- backend/src/test/java/com/duckstar/schedule/ScheduleHandlerTest.java
+++ backend/src/test/java/com/duckstar/schedule/ScheduleHandlerTest.java
@@ -1,38 +1,61 @@
 package com.duckstar.schedule;
 
+import com.duckstar.domain.Anime;
+import com.duckstar.domain.Season;
+import com.duckstar.domain.Week;
+import com.duckstar.domain.mapping.AnimeCandidate;
+import com.duckstar.repository.AnimeCandidate.AnimeCandidateRepository;
+import com.duckstar.repository.SeasonRepository;
+import com.duckstar.repository.Week.WeekRepository;
+import com.duckstar.service.AnimeService;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootTest
 @Disabled("로컬 개발용 테스트")
 @ActiveProfiles("test-db")
 public class ScheduleHandlerTest {
+
     @Autowired
-    ScheduleHandler scheduleHandler;
+    private AnimeService animeService;
+    @Autowired
+    private SeasonRepository seasonRepository;
+    @Autowired
+    private WeekRepository weekRepository;
+    @Autowired
+    private AnimeCandidateRepository animeCandidateRepository;
 
     @Test
     @Transactional
+    @Rollback(false)
     public void scheduler() throws Exception {
-        long start = System.nanoTime();
-
-        //=== 테스트 코드 ===//
+//        long start = System.nanoTime();
 
         //given
         LocalDateTime time = LocalDateTime.of(2025, 9, 21, 22, 0);
+        Season season = seasonRepository.findById(1L).get();
+        Week week = weekRepository.findWeekByStartDateTimeLessThanEqualAndEndDateTimeGreaterThan(time, time).get();
 
         //when
-        scheduleHandler.runSchedule(time);
+        List<Anime> nowShowingAnimes = animeService.getAnimesForCandidate(false, season, time);
+
+        List<AnimeCandidate> animeCandidates = nowShowingAnimes.stream()
+                .map(anime -> AnimeCandidate.create(week, anime))
+                .toList();
+        animeCandidateRepository.saveAll(animeCandidates);
 
         //then
 
-        long end = System.nanoTime();
-        double elapsedSeconds = (end - start) / 1_000_000_000.0;
-        System.out.println("⏰ 실행 시간: " + elapsedSeconds + " seconds");
+//        long end = System.nanoTime();
+//        double elapsedSeconds = (end - start) / 1_000_000_000.0;
+//        System.out.println("⏰ 실행 시간: " + elapsedSeconds + " seconds");
     }
 }
