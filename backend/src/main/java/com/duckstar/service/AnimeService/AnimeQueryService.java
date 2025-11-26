ad5a187b92e212a2d2d6aefc5aac7ca17c1e2f74
Godot-B <bae0821@gmail.com>
2025-11-26 17:06:35 +0900

    refactor: 서비스 CQRS 패턴 적용 (#147)

---
+++ backend/src/main/java/com/duckstar/service/AnimeService/AnimeQueryService.java
@@ -0,0 +1,29 @@
+package com.duckstar.service.AnimeService;
+
+import com.duckstar.domain.Anime;
+import com.duckstar.domain.Season;
+import com.duckstar.domain.mapping.Episode;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static com.duckstar.web.dto.AnimeResponseDto.*;
+import static com.duckstar.web.dto.RankInfoDto.*;
+
+public interface AnimeQueryService {
+    List<DuckstarRankPreviewDto> getAnimeRankPreviewsByWeekId(Long weekId, int size);
+
+    List<RankPreviewDto> getAnimeCornerPreviewsByWeekId(Long weekId, int size);
+
+    List<RankPreviewDto> getAnilabPreviewsByWeekId(Long weekId, int size);
+
+    List<Long> getAllAnimeIds();
+
+    AnimeHomeDto getAnimeHomeDtoById(Long animeId);
+
+    Optional<Episode> findCurrentEpisode(Anime anime, LocalDateTime now);
+
+    // legacy
+    List<Anime> getAnimesForCandidate(Season season, LocalDateTime now);
+}
