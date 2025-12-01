51d6a2d95f916e78c7a27e049f224ade351b309f
Godot-B <bae0821@gmail.com>
2025-12-01 18:32:35 +0900

    modify: 실행 에피소드 Initializer 로직 수정 (#169)

--- backend/src/main/java/com/duckstar/service/EpisodeService/EpisodeCommandServiceImpl.java
+++ backend/src/main/java/com/duckstar/service/EpisodeService/EpisodeCommandServiceImpl.java
@@ -1,10 +1,8 @@
 package com.duckstar.service.EpisodeService;
 
-import com.duckstar.domain.Week;
 import com.duckstar.domain.enums.EpEvaluateState;
 import com.duckstar.domain.mapping.Episode;
 import com.duckstar.repository.Episode.EpisodeRepository;
-import com.duckstar.service.WeekService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,32 +15,22 @@ import java.util.List;
 @Transactional
 public class EpisodeCommandServiceImpl implements EpisodeCommandService {
     private final EpisodeRepository episodeRepository;
-    private final WeekService weekService;
 
     public void updateAllEpisodeStates() {
         LocalDateTime now = LocalDateTime.now();
-        Week week = weekService.getWeekByTime(now);
-        LocalDateTime lastWeekEndedAt = week.getStartDateTime();
-
         List<Episode> allEpisodes = episodeRepository.findAll();
 
         allEpisodes.forEach(episode -> {
             LocalDateTime scheduledAt = episode.getScheduledAt();
             LocalDateTime votingWindowEndedAt = scheduledAt.plusHours(36);
 
-            // 이번 주가 아닌 에피소드
-            if (scheduledAt.isBefore(lastWeekEndedAt)) {
-                episode.setEvaluateState(EpEvaluateState.ALWAYS_OPEN);
-
-            // 이번 주 에피소드
-            } else if (now.isAfter(votingWindowEndedAt)) {
+            if (now.isAfter(votingWindowEndedAt)) {
                 episode.setEvaluateState(EpEvaluateState.LOGIN_REQUIRED);
             } else if (now.isAfter(scheduledAt)) {
                 episode.setEvaluateState(EpEvaluateState.VOTING_WINDOW);
             } else {
                 episode.setEvaluateState(EpEvaluateState.CLOSED);
             }
-
         });
     }
 }
